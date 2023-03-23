482400a2b4fd0c12ad2f2adf16e91695d2248247
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-03-23 21:02:04 +0300

    initial project

---
+++ src/App/index.tsx
@@ -0,0 +1,11 @@
+
+
+
+function App() {
+  return <>
+
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus iste consequuntur alias eos, sunt maxime ullam. Voluptatibus, aliquid. Nihil ab dolorum a ratione unde vero praesentium nobis neque debitis blanditiis.
+  </>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+
+
+import './styles/reset.scss'
+import './styles/common.scss'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <App />
+)
