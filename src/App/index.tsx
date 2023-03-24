457c2bd4444b8feddc627f6b5492c0d6c3104129
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-03-24 06:35:31 +0300

    fix

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,15 +1,18 @@
-
 import Categories from '../components/Categories';
-import Header from '../components/Header/index'
+import Header from '../components/Header/index';
 
 function App() {
-  return <div className="container">
-
-    <Header/>
-    <div className="content">
-        <Categories/>
+  return (
+    <div className="todo">
+      <div className="container">
+        <Header />
+        <div className="content">
+          <Categories />
+          
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
