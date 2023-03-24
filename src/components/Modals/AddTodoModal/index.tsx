30c5b1aff66a334c4b204576802766dd9a3c3594
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-03-24 22:36:41 +0300

    Добавлен метод добавления задачи

--- src/components/Modals/AddTodoModal/index.tsx
+++ src/components/Modals/AddTodoModal/index.tsx
@@ -7,8 +7,8 @@ import { useTodoStore } from '../../../store/store';
 
 const index: React.FC = () => {
   const { modalVisible, setModalVisible } = useTodoStore();
-
-  console.log(modalVisible);
+  const { createTask } = useTodoStore();
+  const [value, setValue] = useState('');
 
   return (
     <>
@@ -16,17 +16,21 @@ const index: React.FC = () => {
         <section className={styles.todoModal}>
           <div className={styles.todoModalTop}>
             <h4 className={styles.todoModalTitle}>Добавить новое дело</h4>
-            <AiOutlineClose className={styles.todoModalClose} onClick={setModalVisible}/>
+            <AiOutlineClose className={styles.todoModalClose} onClick={setModalVisible} />
           </div>
           <div className={styles.todoModalForm}>
             <input
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
               type="text"
               placeholder="введите название дела..."
               className={styles.todoModalInput}
             />
-            <button className={styles.todoModalButton} >
-              добавить
-            </button>
+            <button className={styles.todoModalButton} onClick={() => {
+                createTask(value)
+                setValue('')
+                setModalVisible(false)
+                }}>добавить</button>
           </div>
         </section>
       )}
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,37 @@
-import { create } from 'zustand';
+import  create  from 'zustand';
+import { devtools } from 'zustand/middleware';
+import { generateId } from './helpers';
 
-interface ITodoProps {
-  modalVisible: boolean;
-  setModalVisible: () => void;
+interface Task {
+  id: string;
+  title: string;
+  createdAt: number;
 }
 
-export const useTodoStore = create<ITodoProps>((set) => ({
-  modalVisible: false,
-  setModalVisible: () => set((state) => ({ modalVisible: !state.modalVisible })),
-}));
+interface ToDoStore {
+  modalVisible: boolean;
+  setModalVisible: () => void;
+  tasks: Task[];
+  createTask: (title: string) => void;
+  updateTask?: (id: string, title: string) => void;
+  removeTask?: (id: string) => void;
+}
+
+export const useTodoStore = create<ToDoStore>(
+  devtools((set, get) => ({
+    modalVisible: false,
+    tasks: [],
+    setModalVisible: () => set((state) => ({ modalVisible: !state.modalVisible })),
+    createTask: (title) => {
+      const { tasks } = get();
+      const newTask = {
+        id: generateId(),
+        title: title,
+        createdAt: Date.now(),
+      };
+      set({
+        tasks: [newTask, ...tasks],
+      });
+    },
+  })),
+);
